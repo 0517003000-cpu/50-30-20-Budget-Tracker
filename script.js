02b3f451c58ee398a789a0417da7a166e9f02e67
0517003000-cpu <0517003000@cikal.co.id>
2026-03-13 17:39:12 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,38 @@
+// select all editable cells
+const editableCells = document.querySelectorAll('[contenteditable="true"]');
+
+// load saved data
+window.onload = function () {
+
+let savedData = JSON.parse(localStorage.getItem("budgetData"));
+
+if(savedData){
+
+editableCells.forEach((cell, index) => {
+
+if(savedData[index]){
+cell.innerText = savedData[index];
+}
+
+});
+
+}
+
+};
+
+// save data whenever user edits
+editableCells.forEach((cell, index) => {
+
+cell.addEventListener("input", () => {
+
+let data = [];
+
+editableCells.forEach(c => {
+data.push(c.innerText);
+});
+
+localStorage.setItem("budgetData", JSON.stringify(data));
+
+});
+
+});
